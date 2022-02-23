28efd1bd2fbf8ea9d6b4c8421c2a1690d319895c
Kaylen Kang <kaylen.kang@duke.edu>
2022-02-23 16:24:00 -0500

    In-class exercise with partner

--- post_example.py
+++ post_example.py
@@ -1,10 +1,13 @@
 import requests
 
-out_data = {
-   "name": "Kaylen Kang",
-   "net_id": "kk431",
-   "e-mail": "kaylen.kang@duke.edu"
+
+message = {
+    "user": "kaylenkang117",
+    "message": "Hi what's up"
 }
 
-r = requests.post("http://vcm-21170.vm.duke.edu:5000/student", json=out_data)
+# r = requests.post("http://vcm-21170.vm.duke.edu:5001/add_message", json=message)
+
+
+r = requests.get("http://vcm-21170.vm.duke.edu:5001/get_messages/Jimmy")
 print(r.text)
