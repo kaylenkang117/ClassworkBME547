c4af60b9d2555ab0b0532c697fb37c7ea259ab74
Kaylen Kang <kaylen.kang@duke.edu>
2022-02-23 16:36:34 -0500

    Fixed pycodestyle failures, added coverage

--- post_example.py
+++ post_example.py
@@ -6,7 +6,8 @@ message = {
     "message": "Hi what's up"
 }
 
-# r = requests.post("http://vcm-21170.vm.duke.edu:5001/add_message", json=message)
+r = requests.post(
+    "http://vcm-21170.vm.duke.edu:5001/add_message", json=message)
 
 
 r = requests.get("http://vcm-21170.vm.duke.edu:5001/get_messages/Jimmy")
