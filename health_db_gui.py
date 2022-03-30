eeeeb5b7d4c0c11f218a638ec43b8a4b0d5063f1
Kaylen Kang <kaylen.kang@duke.edu>
2022-03-30 16:41:52 -0400

    Added images

--- health_db_gui.py
+++ health_db_gui.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
+
+from PIL import Image, ImageTk
 
 
 def verify_GUI_inputs(input_id):
@@ -34,6 +37,16 @@ def main_window():
                                                       entered_blood_type)
         # Update interface based on results
         status_label.configure(text=status_string)
+
+    def image_cmd():
+        filename = filedialog.askopenfilename()
+        if filename == "":
+            return
+        pil_image_raw = Image.open(filename)
+        pil_image = pil_image_raw.resize((200, 200))
+        tk_image = ImageTk.PhotoImage(pil_image)
+        image_label.configure(image=tk_image)
+        image_label.image = tk_image
 
     # Create root/base window
     root = tk.Tk()
@@ -78,11 +91,22 @@ def main_window():
     status_label = ttk.Label(root, text="Status")
     status_label.grid(column=0, row=20)
 
+    # Image
+    pil_image_raw = Image.open("chapel.jpg")
+    pil_image = pil_image_raw.resize((200, 200))
+    tk_image = ImageTk.PhotoImage(pil_image)
+    image_label = ttk.Label(root, image=tk_image)
+    image_label.image = tk_image
+    image_label.grid(column=4, row=0, rowspan=10)
+
     # Buttons
     ttk.Button(root, text="OK", command=ok_cmd).grid(column=1, row=20)
     ttk.Button(root, text="Cancel", command=cancel_cmd).grid(column=2, row=20)
+    ttk.Button(root, text="Change image", command=image_cmd).grid(column=3,
+                                                                  row=20)
 
     # Start GUI
+    root.after(2000, image_cmd)
     root.mainloop()
 
 
