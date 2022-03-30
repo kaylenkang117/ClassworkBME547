876b6713b80fec6eaf1b4ffc46610ebf68bbc6fd
Kaylen Kang <kaylen.kang@duke.edu>
2022-03-30 15:06:52 -0400

    Connected GUI to client code

--- health_db_client.py
+++ health_db_client.py
@@ -2,11 +2,15 @@ import requests
 
 server = "http://127.0.0.1:5000"
 
-new_patient = {"name": "Chris", "id": 203, "blood_type": "O+"}
-r = requests.post(server + "/add_patient", json=new_patient)
-print(r.status_code)
-print(r.text)
 
-r = requests.get(server + "/get_results/101")
-print(r.status_code)
-print(r.text)
+def upload_patient_data_to_server(patient_name, patient_id,
+                                  patient_blood_type):
+    new_patient = {"name": patient_name,
+                   "id": patient_id,
+                   "blood_type": patient_blood_type}
+    r = requests.post(server + "/add_patient", json=new_patient)
+    return r.text
+
+# r = requests.get(server + "/get_results/101")
+# print(r.status_code)
+# print(r.text)
